5790c155091181789beb304eeaf089a674b4835c
shenpeng <396635614@qq.com>
2024-01-11 11:02:22 +0800

    fix:修改api

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-  testHost: "http://localhost:8000/",
-  prodHost: "http://localhost:8000/",
+  testHost: "https://rss.900730.xyz:8000/",
+  prodHost: "https://rss.900730.xyz:8000/",
   url: "https://splashy-young-watercress.glitch.me/",
 }
 export default config
